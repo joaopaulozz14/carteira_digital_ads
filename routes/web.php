27cd8f80981633b59927933da6826f15937e47fd
joaopaulozz14 <jp.cm@hotmail.com>
2026-09-16 15:17:26 -0400

    feat(auth): redirect to login page after session destruction

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\CertificadoController;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Auth/Login', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
